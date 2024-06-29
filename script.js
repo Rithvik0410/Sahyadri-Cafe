17acdc324f0484bdf0eca36acfeeacdc27680760
Rithvik Shetty <134826767+Rithvik0410@users.noreply.github.com>
2024-06-29 10:45:47 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const productCards = document.querySelectorAll('.product-card');
-    productCards.forEach(card => {
-        card.addEventListener('click', function() {
-            window.location.href = card.getAttribute('data-url');
-        });
-    });
-});
